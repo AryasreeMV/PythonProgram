013cc7714e94b36a45421d2a5fd15018ef113c5f
AryasreeMV <44431976+AryasreeMV@users.noreply.github.com>
2026-06-19 20:43:27 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,15 @@
+# Program to perform basic arithmetic operations and string concatenation
+
+# Fetching two user inputs and storing them as integers
+num1 = int(input("Enter first number : ")) 
+num2 = int(input("Enter second number : "))
+
+# Performing arithmetic operations and printing the results
+print("Sum is: ",num1+num2) 
+print("Difference is: ", num1-num2)
+print("Product is: ", num1*num2)
+print("Quotient is: ", num1/num2)
+
+
+# String concatenation after type conversion to string
+print("Concatenated string is: ", str(num1) + str(num2))
